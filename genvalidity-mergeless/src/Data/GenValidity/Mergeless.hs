0163dd74d0a2a089cb58f4f75a8609d5d69bbc6f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-06-16 03:42:41 +0200

    Updated to a newer version of validity

--- genvalidity-mergeless/src/Data/GenValidity/Mergeless.hs
+++ genvalidity-mergeless/src/Data/GenValidity/Mergeless.hs
@@ -8,69 +8,71 @@ import Data.GenValidity.Time ()
 
 import Data.Mergeless
 
-instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) =>
-         GenUnchecked (Store i a)
+instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) => GenUnchecked (Store i a)
 
 instance (GenValid i, GenValid a, Ord i, Ord a) => GenValid (Store i a) where
-    genValid = genValidStructurally
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a, Ord i, Ord a) =>
-         GenInvalid (Store i a)
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a, Ord i, Ord a) => GenInvalid (Store i a)
 
 instance (GenUnchecked i, GenUnchecked a) => GenUnchecked (StoreItem i a)
 
 instance (GenValid i, GenValid a) => GenValid (StoreItem i a) where
-    genValid = genValidStructurally
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a) => GenInvalid (StoreItem i a)
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a) =>
+         GenInvalid (StoreItem i a)
 
 instance GenUnchecked a => GenUnchecked (Added a)
 
 instance GenValid a => GenValid (Added a) where
-    genValid = genValidStructurally
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance GenInvalid a => GenInvalid (Added a)
+instance (GenUnchecked a, GenInvalid a) => GenInvalid (Added a)
 
 instance (GenUnchecked i, GenUnchecked a) => GenUnchecked (Synced i a)
 
 instance (GenValid i, GenValid a) => GenValid (Synced i a) where
-    genValid = genValidStructurally
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a) => GenInvalid (Synced i a)
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a) =>
+         GenInvalid (Synced i a)
 
-instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) =>
-         GenUnchecked (SyncRequest i a)
+instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) => GenUnchecked (SyncRequest i a)
 
-instance (GenValid i, GenValid a, Ord i, Ord a) =>
-         GenValid (SyncRequest i a) where
-    genValid = genValidStructurally
+instance (GenValid i, GenValid a, Ord i, Ord a) => GenValid (SyncRequest i a) where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a, Ord i, Ord a) =>
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a, Ord i, Ord a) =>
          GenInvalid (SyncRequest i a)
 
-instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) =>
-         GenUnchecked (SyncResponse i a)
+instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) => GenUnchecked (SyncResponse i a)
 
-instance (GenValid i, GenValid a, Ord i, Ord a) =>
-         GenValid (SyncResponse i a) where
-    genValid = genValidStructurally
+instance (GenValid i, GenValid a, Ord i, Ord a) => GenValid (SyncResponse i a) where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a, Ord i, Ord a) =>
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a, Ord i, Ord a) =>
          GenInvalid (SyncResponse i a)
 
 instance GenUnchecked a => GenUnchecked (CentralItem a)
 
 instance GenValid a => GenValid (CentralItem a) where
-    genValid = genValidStructurally
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance GenInvalid a => GenInvalid (CentralItem a)
+instance (GenUnchecked a, GenInvalid a) => GenInvalid (CentralItem a)
 
-instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) =>
-         GenUnchecked (CentralStore i a)
+instance (GenUnchecked i, GenUnchecked a, Ord i, Ord a) => GenUnchecked (CentralStore i a)
 
-instance (GenValid i, GenValid a, Ord i, Ord a) =>
-         GenValid (CentralStore i a) where
-    genValid = genValidStructurally
+instance (GenValid i, GenValid a, Ord i, Ord a) => GenValid (CentralStore i a) where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
 
-instance (GenInvalid i, GenInvalid a, Ord i, Ord a) =>
+instance (GenUnchecked i, GenUnchecked a, GenInvalid i, GenInvalid a, Ord i, Ord a) =>
          GenInvalid (CentralStore i a)
