7116c8ce9949b94346e2388fe812532be371d91a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-12-03 10:48:37 +0100

    Upgrade to nixos-25.11 and lts-24.16

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "mergeless";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+    nixpkgs-25_05.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
@@ -24,6 +25,7 @@
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-25_05
     , nixpkgs-24_11
     , nixpkgs-24_05
     , nixpkgs-23_11
@@ -62,6 +64,7 @@
           backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).mergelessRelease;
           allNixpkgs = {
             inherit
+              nixpkgs-25_05
               nixpkgs-24_11
               nixpkgs-24_05
               nixpkgs-23_11;
